3fea25cfc02bdf173627043100da24468c744d5e
Joel Sheppard <32169940+sheppduck@users.noreply.github.com>
2018-03-02 15:32:24 -0500

    template test working (#3)

--- perceiver/template.sh
+++ perceiver/template.sh
@@ -14,7 +14,7 @@ sanity_check() {
 createTemplate() {
   NS=php
   #NEW_APP=$1
-  PODS=$2
+  PODS=$1
   #my_pod=$1
   # Create the project to deploy to
   # First let's see if the project exists, nuke it if so
@@ -27,40 +27,43 @@ createTemplate() {
   echo "Test: Deploy Image via OpenShift Template"
   oc new-project $NS
   oc new-app -f /usr/share/openshift/examples/quickstart-templates/rails-postgresql.json
-  echo "PODs variable is: $PODS"
-  sleep 30
-  until [[ `oc get pods | grep -v STATUS | wc -l` -ge "$PODS" ]] ; do
-      echo "[createTemplate]: WAiting on POD9s) to come up, so far: `oc get pods | grep -v NAME`"
-      sleep 3
+  until [[ `oc get pods | grep rails-postgresql-example-1-hook-pre | grep Completed` ]] ; do
+        echo "[createTemplate]: Waiting on PODs to come up, so far: `oc get pods | grep -v NAME`"
+        sleep 1
   done
-  echo "Done waiting!"
-  for i in `oc get pods | grep -v build | grep -v deploy | grep -v NAME | cut -d ' ' -f 1` ; do
+  sleep 10
+  echo "Waiting for the REAL rails-postgresql-1-<random> to come up..."
+  echo -e "`oc get pods` \nIf we got here, then: Done waiting!"
+  for i in `oc get pods | grep -v build | grep -v deploy | grep -v NAME | grep Running | cut -d ' ' -f 1` ; do
+      echo "[[DEBUGGIUNG]]: DOLLAR-EYE is: $i"
       tstAnnotate $i
       retVal=$?
       passed=0
       failed=0
       if [[ retVal -gt 0 ]] ; then
           echo "FAIL: [createTemplate] Failed POD Annotations Test on $i.  Failing Fast!"
+          exit 46
+          echo "[[DEBUGGING]]: "$?""
           (( failed++ ))
       else
-          echo "PAssed POD Annotations test on $i!"
+          echo "Passed POD Annotations test on $i!"
           (( passed++ ))
       fi
   done
-  echo "[createTemplate] Test Passed for all PODs in $NEW_APP"
+  echo "[createTemplate] Test Passed for all PODs!"
 }
 
 tstAnnotate() {
-  my_pod=$1
-  echo "New testing POD Annotations on: $my_pod"
+  PODS=$1
+  echo "New testing POD Annotations on: $PODS"
   echo "Checking for BlackDuck POD Annotations..."
-  a_state=$(oc describe pod $my_pod | grep BlackDuck)
+  a_state=$(oc describe pod $PODS | grep BlackDuck)
   echo "$a_state"
   if [[ -z a_state ]] ; then
-      echo "ERROR: There appears to be no POD Annotations present on $my_pod"
+      echo "ERROR: There appears to be no POD Annotations present on $PODS"
       exit 1;
   else
-      echo "PASS: BlackDuck OpsSight POD Annotations found on POD: $my_pod!  TEST PASS"
+      echo "PASS: BlackDuck OpsSight POD Annotations found on POD: $PODS!  TEST PASS"
   fi
 }
 
