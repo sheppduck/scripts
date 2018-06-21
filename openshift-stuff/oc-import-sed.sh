bd8ea4787bd000e2f327596d7aae92d4185b160e
sheppduck <shepp67@gmail.com>
2018-06-21 14:40:55 -0400

    cleanup and such

--- openshift-stuff/oc-import-sed.sh
+++ openshift-stuff/oc-import-sed.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+#============================================
+##  Credits:  Joel Effing Sheppard -  2018 ##
+#============================================
+
+#==========================================================================================
+##      Purpose:  This script takes an input file that has N docker repo/image names     ##
+##      and then using sed adds an OpenShift command (a prefix and a suffix) so that     ##
+##      one can easily deploy HUNDREDS or THOUSANDS of docker images to OpenShift        ##
+##      with ease.  The input file is created by using a 'docker search' command         ##
+##      in conjunction with the Linux 'cut' command. Here's an example docker search     ##
+##  docker search zip --limit 100 | cut -d ' ' -f1 | grep -v NAME > /path/to/a/file.txt  ##
+#==========================================================================================
 
 #========================
 ##   Setup variables   ##
 #========================
 readonly IMGS_INFILE=$(cat "$1" | wc -l)
 readonly IN_FILE="$1"
+
 #========================================================
-#   Compare how many lines exist in the text file      ##
-#   to the prefix being added by sed, we only want     ##
-#   to add a prefix to what is in the file already     ##
-#   Not infinitely keep adding a prefix and a new line ##
+##     Take the infile and add the prefix using sed    ##
 #========================================================
 ocPrefix() {
     echo "** There are: $IMGS_INFILE images in "$1" **"
@@ -19,9 +29,9 @@ ocPrefix() {
     set +x
 }
 
-#==========================================================
-#     FIX THIS MESS BELOW ONCE FIGURE OUT THE ABOVE PUZZLE!
-#==========================================================
+#=============================================
+##     Now add the prefix also using sed    ##
+#=============================================
 ocSuffix() {
     set -x
     echo "Adding Suffix to all images in "$!" NOW!!"
@@ -29,5 +39,8 @@ ocSuffix() {
     set +x
 }
 
+#===========================
+##  Release the Hounds!!  ##
+#===========================
 ocPrefix
 ocSuffix
