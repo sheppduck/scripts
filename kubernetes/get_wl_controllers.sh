8643505d7671195e92b25b696caa660f71cf48e4
Joel Sheppard <32169940+sheppduck@users.noreply.github.com>
2020-05-26 18:02:56 -0400

    added jobs and improved output

--- kubernetes/get_wl_controllers.sh
+++ kubernetes/get_wl_controllers.sh
@@ -3,12 +3,14 @@
 deployments="$(kubectl get deployments --all-namespaces | awk '$5 > 0 {print $2}' |sed "1 d" | wc -l)"
 ds="$(kubectl get ds --all-namespaces | awk '$5 > 0 {print $2}'| sed "1 d" | wc -l)"
 statefulsets="$(kubectl get statefulsets --all-namespaces | sed "1 d" | wc -l)"
+jobs="$(kubectl get jobs --all-namespaces | grep -v "istio-system" | sed "1 d" | wc -l)"
 get_wl () {
-      total="$(( $ds + $deployments + $statefulsets ))"
-      echo "$deployments"
-      echo "$ds"
-      echo "$statefulsets"
-      echo "$total"
+      total="$(( $ds + $deployments + $statefulsets + $jobs ))"
+      echo "$deployments Deployments"
+      echo "$ds DaemonSets"
+      echo "$statefulsets StatefulSets"
+      echo "$jobs Jobs"
+      echo "$total Total WL Controllers"
 }
 
 get_wl
